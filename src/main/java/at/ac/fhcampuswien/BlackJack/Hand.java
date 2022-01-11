4249e2319375271dcb8d4bf5e8ac6a56814bb765
IshakSij <ishak.sij@hotmail.com>
2022-01-11 17:22:02 +0100

    By Ishak - "Hand" edited, 11.01.2022

--- src/main/java/at/ac/fhcampuswien/BlackJack/Hand.java
+++ src/main/java/at/ac/fhcampuswien/BlackJack/Hand.java
@@ -1,4 +1,34 @@
 package at.ac.fhcampuswien.BlackJack;
 
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+
 public class Hand {
+
+    private ObservableList<Node> cards;
+    private SimpleIntegerProperty worth = new SimpleIntegerProperty(0);
+    private int cardCount;
+    private int aces = 0;
+
+    public Hand(ObservableList<Node> cards) {
+        this.cards = cards;
+    }
+
+    public void addCard(Card card) {
+        //if sum > 21 then Ace worth 1 instead of 11
+        //else
+        cardCount++;
+        worth.set(worth.get() + card.worth);
+    }
+
+    public void reset() {
+        cards.clear();
+        worth.set(0);
+    }
+
+    public SimpleIntegerProperty worthProperty() {
+        return worth;
+    }
 }
